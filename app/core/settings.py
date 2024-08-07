6745013637814f0851d6a0646e9a7e131f09938e
blackxavier <ogbuchiarinze01@gmail.com>
2024-08-07 20:14:11 +0100

    Before trying out something

--- app/core/settings.py
+++ app/core/settings.py
@@ -110,3 +110,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "student.pagination.StudentPagination",
+    "PAGE_SIZE": 10,
+}
